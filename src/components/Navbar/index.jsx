a565d7dc887c7bc4fd6b5744e108303f51ac5143
johnwav <102820657+johnwav@users.noreply.github.com>
2023-02-17 15:27:47 +0100

    Feat: Items in cart will now relfect in cart

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import styles from "./Navbar.module.css";
 import { Logo, CartIcon, Profile } from "./assets";
 import { useSelector } from "react-redux";
+import { data } from "../../app/data.js";
 export const Navbar = () => {
-
-  const [isActive, setIsActive] = useState(true)
-  const handleClick = event => {
-    setIsActive(current => !current)
-  }
-  const count = useSelector((state)=> state.itemsInCart.value)
+  const [isActive, setIsActive] = useState(true);
+  const handleClick = (event) => {
+    setIsActive((current) => !current);
+  };
+  const count = useSelector((state) => state.itemsInCart.value);
 
   return (
     <nav className={styles.container}>
@@ -29,17 +29,30 @@ export const Navbar = () => {
           <div>{count}</div>
         </button>
         <button onClick={handleClick}>
-          <img src={Profile.img} width={"50px"}alt={Profile.alt}></img>
+          <img src={Profile.img} width={"50px"} alt={Profile.alt}></img>
         </button>
       </div>
-    
-      <div className={isActive? styles.toggle : styles.cartdropdown} >
+
+      <div className={isActive ? styles.toggle : styles.cartdropdown}>
         <div className={styles.cartLabel}>Cart</div>
         <div className={styles.line}></div>
-        <div className={styles.cartProductInfo}></div>
+        {count > 0 ? (
+          <div className={styles.cartInfo}>
+            <div>image</div>
+            <div>
+              <p>{data.title}</p>
+              <p>
+                ${data.price} x{count} <strong>${data.price * count}</strong>
+              </p>
+              <div>dlt</div>
+            </div>
+          </div>
+        ) : (
+          <div>Your cart is empty.</div>
+        )}
+
         <button className={styles.checkoutBtn}>Checkout</button>
       </div>
-  
     </nav>
   );
 };
